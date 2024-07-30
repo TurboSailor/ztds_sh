e20167e4d1a1921f872cb110838330af1a1c3d81
TurboSailor <157695119+TurboSailor@users.noreply.github.com>
2024-07-30 17:34:15 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -132,7 +132,7 @@ mkdir -m 777 /var/www/html/$domain
 
 rm -rf /tmp/ztds
 rm -f /tmp/ztds.7z
-curl -L -o /tmp/ztds.7z https://raw.githubusercontent.com/TurboSailor/ztds_sh/main/$ztds_version.7z?raw=true
+curl -L -o /tmp/ztds.7z https://raw.githubusercontent.com/TurboSailor/ztds_sh/main/ztds_v.0.8.4.7z
 7za x -o/tmp/ztds /tmp/ztds.7z
 cp -a /tmp/ztds/$ztds_version/. /var/www/html/$domain
 chmod 777 -R /var/www/html/$domain
